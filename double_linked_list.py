2da3a310748ffd926fdca6504a0ff7c5feb6ac1e
Aman <amankumar.edith@gmail.com>
2019-12-03 18:06:56 +0530

    Implemented doubly linked list

--- double_linked_list.py
+++ double_linked_list.py
@@ -2,10 +2,11 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+        self.prev = None
 
 
-class SinglyLinkedList:
-    def __init__(self, list=None):
+class DoublyLinkedList:
+    def __init__(self, list = None):
         """
         Creates a linked list with the elements of list as its nodes
             :params: - 
@@ -17,16 +18,15 @@ class SinglyLinkedList:
         self.num_elements = 0
 
         # adding list items as nodes
-        if list is not None:
-            for list_item in list:
-                self.append(list_item)
+        for item in list:
+            self.append(item)
 
     def size(self):
         """ 
         Returns the size of list
-        :params: - None
-        :output: -
-            -size - the number of nodes in the list
+            :params: - None
+            :output: -
+                -size - the number of nodes in the list
         Time complexity - O(1)
         """
         return self.num_elements
@@ -43,10 +43,10 @@ class SinglyLinkedList:
     def to_list(self):
         """
         Returns the list representation of a linked list
-        :params: - None
-        :output:
-            list - list representation of linked list
-        Time complexity - O(n), assuming that append operation in python has a complexity of O(1)
+            :params: - None
+            :output:
+                list - list representation of linked list
+        Time complexity = O(n) assuming time complexity of list.append() is O(1)
         """
         list = []
         current_node = self.head
@@ -63,14 +63,13 @@ class SinglyLinkedList:
             :return: - None
         Time complexity - O(1)
         """
-        # list is empty, both head and tail need to be updated
         if self.head is None:
             self.head = Node(value)
             self.tail = self.head
 
-        # list is not empty, only tail needs to be updated
         else:
             self.tail.next = Node(value)
+            self.tail.next.prev = self.tail
             self.tail = self.tail.next
 
         self.num_elements += 1
@@ -83,17 +82,15 @@ class SinglyLinkedList:
             :return: - None
         Time complexity - O(1)
         """
-        # list is empty, both head and tail need to be updated
         if self.head is None:
             self.head = Node(value)
             self.tail = self.head
 
-        # list is not empty, only head needs to be updated
         else:
-            temp = Node(value)
-            temp.next = self.head
-            self.head = temp
-
+            self.head.prev = Node(value)
+            self.head.prev.next = self.head
+            self.head = self.head.prev
+        
         self.num_elements += 1
 
     def insert(self, index, value):
@@ -105,27 +102,37 @@ class SinglyLinkedList:
             :output: - None
         Time complexity - O(n)
         """
-        # if index is greater than size, we append, if it is less than 0, we prepend
-        if index > self.num_elements-1:
-            index = self.num_elements-1
+        if index > (self.num_elements - 1):
+            index = self.num_elements - 1
         elif index < 0:
             index = 0
 
-        # inserting the node
         if index == 0:
             return self.prepend(value)
         elif index == self.num_elements - 1:
             return self.append(value)
         else:
+            # as the counter = 0 case is already covered
             counter = 1
-            current_node = self.head
-            while current_node.next is not None:
-                if counter == index:
-                    temp = current_node.next
-                    current_node.next = Node(value)
-                    current_node.next.next = temp
+            # pointer to the second node
+            current_node = self.head.next
+            while current_node is not None:
+                if index == counter:
+                    # node before new node
+                    next_node = current_node
+                    # node after new node
+                    prev_node = current_node.prev
+
+                    # manipulating references
+                    prev_node.next = Node(value)
+                    prev_node.next.prev = prev_node
+                    prev_node.next.next = next_node
+                    next_node.prev = prev_node.next
+
+                    # incrementing node count and returning
                     self.num_elements += 1
                     return
+
                 counter += 1
                 current_node = current_node.next
 
@@ -134,7 +141,7 @@ class SinglyLinkedList:
         Removes the last node and returns its value
             :params: - None
             :output: - value of node removed and None if list is empty
-        Time complexity - O(n)
+        Time complexity - O(1)
         """
         if self.num_elements == 0:
             return None
@@ -143,16 +150,16 @@ class SinglyLinkedList:
             self.head = None
             self.tail = None
         else:
-            prev_node = self.head
-            current_node = prev_node.next
-            while current_node.next is not None:
-                current_node = current_node.next
-                prev_node = prev_node.next
-            value = current_node.value
-            prev_node.next = None
+            value = self.tail.value
+            # storing a reference to the new tail node
+            temp = self.tail.prev
+            self.tail.prev = None
+            self.tail = temp
+            self.tail.next = None
+        
         self.num_elements -= 1
         return value
-
+            
     def remove_from_beginning(self):
         """
         Removes the first node and retursn its value
@@ -168,11 +175,15 @@ class SinglyLinkedList:
             self.tail = None
         else:
             value = self.head.value
-            self.head = self.head.next
-
+            # storing a reference to the new head node
+            temp = self.head.next
+            self.head.next = None
+            self.head = temp
+            self.head.prev = None
+        
         self.num_elements -= 1
         return value
-
+        
     def remove(self, index):
         """
         Removes the node at specified index and returns its value
@@ -181,28 +192,36 @@ class SinglyLinkedList:
             :ouput: - value of the removed node and None if that index has no node
         Time complexity - O(n)
         """
-        # adjusting index value
-        if index > self.num_elements - 1:
+        if index > (self.num_elements - 1):
             index = self.num_elements - 1
         elif index < 0:
             index = 0
 
-        # removing the node
         if index == 0:
             return self.remove_from_beginning()
-        elif index == self.num_elements - 1:
+        elif index == self.num_elements -1 :
             return self.remove_from_end()
         else:
             counter = 1
-            current_node = self.head
-            while current_node.next is not None:
-                if counter == index:
-                    value = current_node.next.value
-                    current_node.next = current_node.next.next
+            current_node = self.head.next
+            while current_node is not None:
+                if index == counter:
+                    value= current_node.value
+                    prev_node = current_node.prev
+                    next_node = current_node.next
+
+                    prev_node.next = current_node.next
+                    next_node.prev = current_node.prev
+
+                    # not necessary
+                    current_node.prev = None
+                    current_node.next = None
+
+                    # decrementing node count and returning the value of the removed node
                     self.num_elements -= 1
                     return value
-                current_node = current_node.next
-                counter += 1
+
+
 
     def __str__(self):
         """
@@ -210,13 +229,19 @@ class SinglyLinkedList:
             :params: - None
             :output: - 
                 value - (string) - string representation of list
-        Time complexity - 
-            self.to_list() to get list representation of list - O(n)
-            str(item) for each list item - O(n)
-            string.join() - O(n)
-        overall ~= O(n)
+        Time complexity - O(n)
         """
         return ", ".join([str(item) for item in self.to_list()])
+
+    def __repr__(self):
+        """
+        Returns the string representation of a list
+            :params: - None
+            :output: - 
+                value - (string) - string representation of list
+        Time complexity - O(n)
+        """
+        return self.__str__()
 
     def search(self, value):
         """
@@ -232,7 +257,7 @@ class SinglyLinkedList:
             if current_node.value == value:
                 return True
             current_node = current_node.next
-        return False
+        return None
     
     def get(self, index):
         """
@@ -255,12 +280,4 @@ class SinglyLinkedList:
             counter += 1
         return None
 
-    def __repr__(self):
-        """
-        Returns the string representation of the list by calling __str__()
-            :params: - None
-            :output: -
-                string - string representation of linked list
-        Time complexity ~ O(n) as it uses __str__() internally
-        """
-        return self.__str__()
+linked_list = DoublyLinkedList()
