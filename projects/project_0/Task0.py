d8d296af76eb0a8408a4daf217499aa3da98947d
Aman <amankumar.edith@gmail.com>
2019-12-08 08:18:25 +0530

    Completed task_0 of project_0

--- projects/project_0/Task0.py
+++ projects/project_0/Task0.py
@@ -3,14 +3,13 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files.
 """
 import csv
-with open('texts.csv', 'r') as f:
+with open('./texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
 
-with open('calls.csv', 'r') as f:
+with open('./calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
-
 
 """
 TASK 0:
@@ -20,3 +19,16 @@ Print messages:
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
 
+# i am not considering the time required to read from the files and store the data in lists
+
+# constant time to read the records, as len() has O(1) complexity
+first_text_record = texts[0]
+last_call_record = calls[len(calls) - 1]
+
+# format() scans a text and does replacements, the replacements will be as much as the number of arguments for format()
+print("First record of texts, {} texts {} at time {}".format(
+    first_text_record[0], first_text_record[1], first_text_record[2]))
+print("Last record of calls, {} calls {} at time {}, lasting {} seconds".format(
+    last_call_record[0], last_call_record[1], last_call_record[2], last_call_record[3]))
+
+# so, overall, my part of code will have a linear complexity, O(1)
