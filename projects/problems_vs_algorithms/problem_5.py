1241dd5ec629bd983711036b3bbf8c3c4dfde77d
Aman <amankumar.edith@gmail.com>
2019-12-29 16:10:07 +0530

    Fixed bug with autocomplete implementation and added edge cases

--- projects/problems_vs_algorithms/problem_5.py
+++ projects/problems_vs_algorithms/problem_5.py
@@ -5,11 +5,18 @@ class TrieNode:
 
     def suffixes(self, suffix=''):
         result = []
+
+        # if we are at the text that user typed, and it is a word itself, then we add it to result and look for it's children
         if self.is_word and suffix != '':
             result.append(suffix)
 
+        # if it is a leaf node
+        if len(self.children) == 0:
+            return result
+
         for char in self.children:
-            result.extend(self.children[char].suffixes(suffix + char))
+            result.extend(self.children[char].suffixes(suffix=suffix+char))
+
         return result
 
 
@@ -64,17 +71,18 @@ wordList = [
 for word in wordList:
     MyTrie.insert(word)
 
-# getting reference to trie nodes to imitate that we have typed this much and autocomplete suggestions are based on what can come afterwards
-an_trie_node = MyTrie.find("an")
-fu_trie_node = MyTrie.find("fu")
-fun_trie_node = MyTrie.find("fun")
-all_possibilities = MyTrie.root
 
-print(an_trie_node.suffixes())
-# ['t', 'thology', 'tagonist', 'tonym']
-print(fu_trie_node.suffixes())
-# ['n', 'nction']
-print(fun_trie_node.suffixes())
-# ['ction']
-print(all_possibilities.suffixes())
-# ['ant', 'anthology', 'antagonist', 'antonym', 'fun', 'function', 'factory', 'trie', 'trigger', 'trigonometry', 'tripod']
+def test_function(trie_node, word_list):
+    if trie_node is None:
+        return word_list == []
+    else:
+        return trie_node.suffixes() == word_list
+
+
+print("Pass" if test_function(MyTrie.find("an"), [
+      't', 'thology', 'tagonist', 'tonym']) else "Fail")
+print("Pass" if test_function(MyTrie.find("fu"), ['n', 'nction']) else "Fail")
+print("Pass" if test_function(MyTrie.find("fun"), ['ction']) else "Fail")
+print("Pass" if test_function(MyTrie.find("ant"), ['hology', 'agonist', 'onym']) else "Fail")
+print("Pass" if test_function(MyTrie.root, ['ant', 'anthology', 'antagonist', 'antonym', 'fun', 'function', 'factory', 'trie', 'trigger', 'trigonometry', 'tripod']) else "Fail")
+print("Pass" if test_function(MyTrie.find("you can't find me"), []) else "Fail")
