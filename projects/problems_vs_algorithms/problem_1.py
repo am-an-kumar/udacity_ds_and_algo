b9ff914b13e641a82969f58816212d88b699c8f3
Aman <amankumar.edith@gmail.com>
2019-12-29 15:20:55 +0530

    Added edge cases for square root algorithm

--- projects/problems_vs_algorithms/problem_1.py
+++ projects/problems_vs_algorithms/problem_1.py
@@ -4,9 +4,12 @@ def sqrt(number):
         Args:
             number(int): Number to find the floored squared root
         Returns:
-            int: Floored Square Root
+            int: Floored Square Root/ None for invalid input
     Time complexity - O(log(n))
     """
+    if not isinstance(number, int) or number < 0:
+        return None
+
     if (number == 0) or (number == 1):
         return number
 
@@ -34,3 +37,6 @@ print("Pass" if (0 == sqrt(0)) else "Fail")
 print("Pass" if (4 == sqrt(16)) else "Fail")
 print("Pass" if (1 == sqrt(1)) else "Fail")
 print("Pass" if (5 == sqrt(27)) else "Fail")
+print("Pass" if (None == sqrt("added edge cases")) else "Fail")
+print("Pass" if (None == sqrt(None)) else "Fail")
+print("Pass" if (None == sqrt(-27)) else "Fail")
